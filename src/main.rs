fe2fee99fdfaa4b7e9c296632f74a0f91e0b844f
aanubhav04@gmail.com <anubhav@Anubhavs-MacBook-Air.local>
2024-08-16 11:03:34 +0530

    migrated from logs to tracing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use env_logger::Env;
 use sqlx::{Connection, PgConnection, PgPool};
 use zero_to_prod::configuration::get_configuration;
 use zero_to_prod::startup::run;
@@ -6,6 +7,7 @@ use zero_to_prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use std::net::TcpListener;
 use std::sync::Arc;
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
+use actix_web::middleware::Logger;
 use sqlx::{PgConnection, PgPool};
 use crate::{greet, health_check};
 use crate::routes::subscribe;
@@ -10,12 +11,14 @@ pub  fn run(listener: TcpListener,db_pool: PgPool) -> Result<Server,std::io::Err
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new( move|| {
         App::new()
+            .wrap(Logger::default())
             .route("/",web::get().to(greet))
             .route("/greet/{name}",web::get().to(greet))
             .route("/health_check",web::get().to(health_check))
             .route("/subscriptions",web::post().to(subscribe))
             .app_data(db_pool.clone()) // Register the connection as part of app data
     })
+        .workers(4)
         .listen(listener)?
         .run();
     Ok(server)
